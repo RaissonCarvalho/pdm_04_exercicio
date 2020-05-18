cf77e808b07269fe75570ea4ffa4251913588795
raissoncarvalho <raissoncarvalho94@gmail.com>
2020-05-18 13:46:32 -0300

    commitando exercicio

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/icons8-diamond-100.png'),
+        ),
+      ),
+    ),
+  ));
+}
